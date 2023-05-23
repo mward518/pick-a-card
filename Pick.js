3f386986410457813be6b654c93d964d1e9e1ac7
Matthew Ward <mward@Matthews-MacBook-Pro.local>
2023-05-23 18:45:43 -0500

    it helps to save the code

--- Pick.js
+++ Pick.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
     $.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1", function (data) {
             const suit = data.cards[0].suit;
             const value = data.cards[0].value;
+            const image = data.cards[0].image;
             console.log(suit);
-            console.lot(value);
+            console.log(value);
+            console.log(image);
         });
 });
