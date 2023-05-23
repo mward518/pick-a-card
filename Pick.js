3b8f135d25045ab894e04a680abfa6b5b595d431
Matthew Ward <mward@Matthews-MacBook-Pro.local>
2023-05-23 11:36:27 -0500

    Added our file and logging suit to check if this works

---
+++ Pick.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+    $get("https://deckofcardsapi.com/api/deck/new/draw/?count=1", function (
+        data
+
+    ) {
+        const suit = data.cards[0].suit;
+        console.log(suit);
+    });
+});
